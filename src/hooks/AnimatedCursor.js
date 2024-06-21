d8b1f93be53864eb138b7c1e02ce606749545db7
Audrey <aleong@student.ethz.ch>
2024-06-20 21:15:37 -0700

    changing the cursor to remove shadow animation

--- src/hooks/AnimatedCursor.js
+++ src/hooks/AnimatedCursor.js
@@ -136,25 +136,25 @@ function CursorCore({
     }, [])
   
     // Outer Cursor Animation Delay
-    const animateOuterCursor = useCallback(
-      (time) => {
-        if (previousTimeRef.current !== undefined) {
-          coords.x += (endX.current - coords.x) / trailingSpeed
-          coords.y += (endY.current - coords.y) / trailingSpeed
-          cursorOuterRef.current.style.top = `${coords.y}px`
-          cursorOuterRef.current.style.left = `${coords.x}px`
-        }
-        previousTimeRef.current = time
-        requestRef.current = requestAnimationFrame(animateOuterCursor)
-      },
-      [requestRef] // eslint-disable-line
-    )
+    // const animateOuterCursor = useCallback(
+    //   (time) => {
+    //     if (previousTimeRef.current !== undefined) {
+    //       coords.x += (endX.current - coords.x) / trailingSpeed
+    //       coords.y += (endY.current - coords.y) / trailingSpeed
+    //       cursorOuterRef.current.style.top = `${coords.y}px`
+    //       cursorOuterRef.current.style.left = `${coords.x}px`
+    //     }
+    //     previousTimeRef.current = time
+    //     requestRef.current = requestAnimationFrame(animateOuterCursor)
+    //   },
+    //   [requestRef] // eslint-disable-line
+    // )
   
     // RAF for animateOuterCursor
-    useEffect(() => {
-      requestRef.current = requestAnimationFrame(animateOuterCursor)
-      return () => cancelAnimationFrame(requestRef.current)
-    }, [animateOuterCursor])
+    // useEffect(() => {
+    //   requestRef.current = requestAnimationFrame(animateOuterCursor)
+    //   return () => cancelAnimationFrame(requestRef.current)
+    // }, [animateOuterCursor])
   
     // Mouse Events State updates
     const onMouseDown = useCallback(() => setIsActive(true), [])
@@ -169,27 +169,27 @@ function CursorCore({
     useEventListener('mouseout', onMouseLeaveViewport)
   
     // Cursors Hover/Active State
-    useEffect(() => {
-      if (isActive) {
-        cursorInnerRef.current.style.transform = `translate(-50%, -50%) scale(${innerScale})`
-        cursorOuterRef.current.style.transform = `translate(-50%, -50%) scale(${outerScale})`
-      } else {
-        cursorInnerRef.current.style.transform = 'translate(-50%, -50%) scale(1)'
-        cursorOuterRef.current.style.transform = 'translate(-50%, -50%) scale(1)'
-      }
-    }, [innerScale, outerScale, isActive])
+    // useEffect(() => {
+    //   if (isActive) {
+    //     cursorInnerRef.current.style.transform = `translate(-50%, -50%) scale(${innerScale})`
+    //     cursorOuterRef.current.style.transform = `translate(-50%, -50%) scale(${outerScale})`
+    //   } else {
+    //     cursorInnerRef.current.style.transform = 'translate(-50%, -50%) scale(1)'
+    //     cursorOuterRef.current.style.transform = 'translate(-50%, -50%) scale(1)'
+    //   }
+    // }, [innerScale, outerScale, isActive])
   
     // Cursors Click States
-    useEffect(() => {
-      if (isActiveClickable) {
-        cursorInnerRef.current.style.transform = `translate(-50%, -50%) scale(${
-          innerScale * 1.2
-        })`
-        cursorOuterRef.current.style.transform = `translate(-50%, -50%) scale(${
-          outerScale * 1.4
-        })`
-      }
-    }, [innerScale, outerScale, isActiveClickable])
+    // useEffect(() => {
+    //   if (isActiveClickable) {
+    //     cursorInnerRef.current.style.transform = `translate(-50%, -50%) scale(${
+    //       innerScale * 1.2
+    //     })`
+    //     cursorOuterRef.current.style.transform = `translate(-50%, -50%) scale(${
+    //       outerScale * 1.4
+    //     })`
+    //   }
+    // }, [innerScale, outerScale, isActiveClickable])
   
     // Cursor Visibility State
     useEffect(() => {
@@ -264,19 +264,19 @@ function CursorCore({
         ...(innerStyle && innerStyle),
         transition: 'opacity 0.15s ease-in-out, transform 0.25s ease-in-out'
       },
-      cursorOuter: {
-        zIndex: 999,
-        display: 'block',
-        position: 'fixed',
-        borderRadius: '50%',
-        pointerEvents: 'none',
-        width: outerSize,
-        height: outerSize,
-        backgroundColor: `rgba(${color}, ${outerAlpha})`,
-        transition: 'opacity 0.15s ease-in-out, transform 0.15s ease-in-out',
-        willChange: 'transform',
-        ...(outerStyle && outerStyle)
-      }
+      // cursorOuter: {
+      //   zIndex: 999,
+      //   display: 'block',
+      //   position: 'fixed',
+      //   borderRadius: '50%',
+      //   pointerEvents: 'none',
+      //   width: outerSize,
+      //   height: outerSize,
+      //   backgroundColor: `rgba(${color}, ${outerAlpha})`,
+      //   transition: 'opacity 0.15s ease-in-out, transform 0.15s ease-in-out',
+      //   willChange: 'transform',
+      //   ...(outerStyle && outerStyle)
+      // }
     }
   
     // Hide / Show global cursor
